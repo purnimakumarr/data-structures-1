3de85c8f4630b59635de4a23cb393da3cf817c8f
purnimakumarr <69529208+purnimakumarr@users.noreply.github.com>
2020-10-27 16:10:22 +0530

    Update AVLTrees.cpp

--- AVLTrees.cpp
+++ AVLTrees.cpp
@@ -9,6 +9,8 @@ Write a program to implement AVL Trees. Perform following operations:
 
 using namespace std;
 
+//global declarations
+enum class type{R_0, R_1, R_neg1, L_0, L_1, L_neg1, null};
 //only for formatting
 void print()
 {
@@ -30,10 +32,12 @@ class AVLTree
 public:
 	AVLTree();
 	void insertion();
-	void deletion();
+	void deletion(Node*, int);
+    void delc(Node*&);
     int get_height(Node*);
     void calculate_bf(Node*);
-    void balance_tree(Node*);
+    void balance_tree_after_insertion(Node*);
+    void balance_tree_after_deletion(Node*, type);
     void right_rotate(Node*);
     void left_rotate(Node*);
     void recursive_inorder(Node*);
@@ -96,7 +100,7 @@ void AVLTree::insertion()
             q->parent = p1;
 
             //checks if tree is balance or not, if not then balance the tree using rotations
-            balance_tree(q);
+            balance_tree_after_insertion(q);
         }
         cout << "Enter data to be inserted otherwise, enter zero(zero cannot be inserted in binary tree) : ";
         cin >> item;
@@ -147,8 +151,8 @@ void AVLTree::calculate_bf(Node* temp)
     }
 }
 
-//checks if a tree is balance or not, if not then balances th tree using rotations
-void AVLTree::balance_tree(Node* temp)
+//balances the tree after insertion
+void AVLTree::balance_tree_after_insertion(Node* temp)
 {
     int flag = 0;
     //calculating the height and balanced factor of all the nodes since it might be changed due to insertion of new node
@@ -156,7 +160,7 @@ void AVLTree::balance_tree(Node* temp)
     calculate_bf(root);
 
     for (Node* temp1 = temp->parent; temp1 != NULL; temp1 = temp1->parent)
-        if (temp1->bf < -1 || temp1->bf>1)
+        if (temp1->bf < -1 || temp1->bf > 1)
         {
             p = temp1;
             flag = 1;
@@ -166,23 +170,23 @@ void AVLTree::balance_tree(Node* temp)
     if (flag == 1)
     {
         //right child, right subtree
-        if (p->bf == -2 && p->right->bf == -1)
+        if (p->bf == 2 && p->right->bf == 1)
             left_rotate(p);
         //right child, left subtree
-        else if (p->bf == -2 && p->right->bf == 1)
+        else if (p->bf == 2 && p->right->bf == -1)
         {
             right_rotate(p->right);
             left_rotate(p);
 
         }
         //left child, right subtree
-        else if (p->bf == 2 && p->left->bf == -1)
+        else if (p->bf == -2 && p->left->bf == 1)
         {
             left_rotate(p->left);
             right_rotate(p);
         }
         //left child, left subtree
-        else if (p->bf == 2 && p->left->bf == 1)
+        else if (p->bf == -2 && p->left->bf == -1)
             right_rotate(p);
 
         root->height = get_height(root);
@@ -195,7 +199,8 @@ void AVLTree::left_rotate(Node* x)
 {
     Node* y = x->right;
     x->right = y->left;
-    y->left->parent = x;
+    if(y->left!=NULL)
+        y->left->parent = x;
     y->parent = x->parent;
 
     if (x->parent == NULL)
@@ -214,7 +219,8 @@ void AVLTree::right_rotate(Node *x)
 {
     Node* y = x->left;
     x->left = y->right;
-    y->right->parent = x;
+    if(y->right!=NULL)
+        y->right->parent = x;
     y->parent = x->parent;
 
     if (x->parent == NULL)
@@ -235,12 +241,150 @@ void AVLTree::recursive_inorder(Node* temp)
     if (temp != NULL)
     {
         recursive_inorder(temp->left);
-        cout << "Data : "<<temp->data << " " << "Height : " << temp->height <<" "<< "Balance Factor : " << temp->bf;
+        cout << "Data : "<<temp->data << "\t" << "Height : " << temp->height <<"\t"<< "Balance Factor : " << temp->bf;
         cout << endl;
         recursive_inorder(temp->right);
     }
 }
 
+void AVLTree::deletion(Node* temp, int item)
+{
+    Node* prev = NULL;
+
+    //search the node we want to delete
+    while (temp != NULL)
+    {
+        if (item == temp->data)
+            break;
+
+        prev = temp; //stores address of previous/parent node
+        if (item < temp->data)
+            temp = temp->left;
+        else
+            temp = temp->right;
+    }
+
+    if (temp != NULL && temp->data == item)
+    {
+        //for root node
+        if (temp == root)
+            delc(root);
+        //for all other nodes
+        else if (prev->left == temp)
+            delc(prev->left);
+        else
+            delc(prev->right);
+    }
+    else if (root != NULL)
+        cout << "\nKey not found.";
+    else
+        cout << "\nAVL Tree Empty.";
+}
+
+void AVLTree::delc(Node*& temp1)
+{
+    Node* tmp = temp1, * prev;
+    type rotation;
+
+    //if a node has single or no child
+    if (temp1->right == NULL)
+        temp1 = temp1->left;
+    else if (temp1->left == NULL)
+        temp1 = temp1->right;
+
+    //if a node having 2 children is deleted
+    else
+    {
+        tmp = temp1->left;
+        prev = temp1;
+
+        while (tmp->right != NULL)
+        {
+            prev = tmp;
+            tmp = tmp->right;
+        }
+
+        temp1->data = tmp->data;
+
+        if (prev == temp1)
+            prev->left = tmp->left;
+        else
+            prev->right = tmp->right;
+    }
+
+    rotation = type::null;
+    if (tmp == tmp->parent->right)
+    {
+        //R(0)
+        if (tmp->parent->left->bf == 0)
+            rotation = type::R_0;
+        //R(-1)
+        else if (tmp->parent->left->bf == -1)
+            rotation = type::R_neg1;
+        //R(1)
+        else if (tmp->parent->left->bf == 1)
+            rotation = type::R_1;
+
+    }
+    else if (tmp == tmp->parent->left)
+    {
+        //L(0)
+        if (tmp->parent->right->bf == 0)
+            rotation = type::L_0;
+        //L(-1)
+        else if (tmp->parent->right->bf == -1)
+            rotation = type::L_neg1;
+        //L(1)
+        else if (tmp->parent->right->bf == 1)
+            rotation = type::L_1;
+
+    }
+    Node* r = tmp->parent;
+    delete tmp;
+    balance_tree_after_deletion(r, rotation);
+}
+
+//balances the tree after deletion
+void AVLTree::balance_tree_after_deletion(Node* temp, type rotation)
+{
+    root->height = get_height(root);
+    calculate_bf(root);
+
+    int flag = 0;
+    for (Node* temp1 = temp; temp1 != NULL; temp1 = temp1->parent)
+        if (temp1->bf < -1 || temp1->bf > 1)
+        {
+            p = temp1;
+            flag = 1;
+            break;
+        }
+
+    if (flag == 1)
+    {
+        if (rotation == type::R_0)
+            right_rotate(p);
+        else if (rotation == type::R_neg1)
+            right_rotate(p);
+        else if (rotation == type::R_1)
+        {
+            left_rotate(p->left);
+            right_rotate(p);
+        }
+        else if (rotation == type::L_0)
+            left_rotate(p);
+        else if(rotation == type::L_1)
+            left_rotate(p);
+        else
+        {
+            right_rotate(p->right);
+            left_rotate(p);
+        }
+    }
+
+    root->height = get_height(root);
+    calculate_bf(root);
+
+}
 
 int main()
 {
@@ -261,7 +405,10 @@ int main()
 		{
 		case '1': obj.insertion();
 			break;
-		case '2':
+        case '2': int item;
+            cout << "Enter item you want to delete : ";
+            cin >> item;
+            obj.deletion(root, item);
 			break;
         case '3': cout << "\nAVL Tree (Node Details) :-\n";
             obj.recursive_inorder(root);
